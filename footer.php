984379fc908ba1c8a3bad67cc8fe18695886f6d8
Joseph May <jwmay87@gmail.com>
2024-04-28 11:30:12 -0700

    activate instagram link

--- footer.php
+++ footer.php
@@ -39,7 +39,7 @@
 					<li><a class="twitter" href="https://twitter.com/uwmemc/">Twitter</a></li>
 					<li><a class="youtube" href="https://www.youtube.com/channel/UCR06IU0FJgxmXS0PzJw7OiQ">YouTube</a></li>
 					<!-- <li><a class="facebook" href="https://www.facebook.com/UofWA">Facebook</a></li> -->
-					<!-- <li><a class="instagram" href="https://instagram.com/uofwa">Instagram</a></li> -->
+					<li><a class="instagram" href="https://www.instagram.com/uwmemc/">Instagram</a></li>
 					<!-- <li><a class="linkedin" href="https://www.linkedin.com/company/university-of-washington">LinkedIn</a></li> -->
 					<!-- <li><a class="pinterest" href="https://www.pinterest.com/uofwa/">Pinterest</a></li> -->
 				</ul>
